8372a2265e200d3995343705c604ef180823aaee
Armand-28 <armandopp2809@gamil.com>
2024-01-04 17:10:04 -0500

    Prestamos

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+    const serverUrl= "http://localhost/PRESTAMOS/";
+
+    const company="Sistemas Prestamos";
+
+    const moneda ="$";
+
+    date_default_timezone_set("America/Bogota");
+
+?>
---
+++ config/server.php
@@ -0,0 +1,14 @@
+<?php
+
+    const server = "localhost";
+    const db = "prestamos";
+    const user = "root";
+    const pass = "";
+
+    const sgbd ="mysql:host=".server.";dbname=".db;
+
+    const metodo = "AES-256-CBC";
+    const secret_key = '$Prestamos@2024';
+    const secret_id ='032024';
+
+?>
